d27afad72835d01f1f0e43a8c296a84b58326eb8
gunggme-amuz <gunggme@amuz.co.kr>
2025-02-27 10:24:41 +0900

    refactor : userdata 가입 경로 default 수정

--- models.py
+++ models.py
@@ -195,7 +195,7 @@ class UserData(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     phone_number = Column(String, index=True)
-    register_way = Column(String, default="MANUAL")
+    register_way = Column(String, default="매장가입")
     game_join_count = Column(Integer, default=0)
     visit_count = Column(Integer, default=0)
     register_at = Column(DateTime, default=datetime.now())
